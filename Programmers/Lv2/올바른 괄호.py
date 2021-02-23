8099afe770ea0a1538a3d6270039f1e139da9545
ssongplay <syj4700@gmail.com>
2021-02-24 00:19:27 +0900

    Update 올바른 괄호.py

--- Programmers/Lv2/올바른 괄호.py
+++ Programmers/Lv2/올바른 괄호.py
@@ -1,9 +1,15 @@
-# 이게 아닌가봐
+# https://programmers.co.kr/learn/courses/30/lessons/12909
+# 블로그 - https://velog.io/@ssongplay/프로그래머스-올바른-괄호-Python
 
 def solution(s):
-    answer = True
-    s = list(s)
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return True if s.count('(')==s.count(')') and s[0]==('(') and s[-1]==(')') else False
+    stack = []
+    for i in s:
+        if i == '(':  # '('는 stack에 추가
+            stack.append(i)
+        else:  # i == ')'인 경우
+            if stack == []:  # 괄호 짝이 ')'로 시작하면 False 반환
+                return False
+            else:
+                stack.pop()  # '('가 ')'와 짝을 이루면 stack에서 '(' 하나 제거
+    
+    return True if stack==[] else False
